05b6d443bb05b77702c2a582a9436eaeaf751b1b
hercilio17t <136920405+hercilio17t@users.noreply.github.com>
2024-11-24 20:57:39 -0300

    Exercício 4
    Exercícios FUNÇÕES concluído

--- src/exercicio-04/verificarPremiado.ts
+++ src/exercicio-04/verificarPremiado.ts
@@ -1,3 +1,36 @@
-// IMPLEMENTE AQUI A SUA FUNÇÃO
+/*
+Problema
+Você está desenvolvendo um programa que irá premiar com um carro a pessoa que tiver comprado a cartela com o número sorteado.
+
+Tarefa
+Você deve criar uma função chamada verificarPremiado, que recebe como parâmetro duas variáveis
+
+numeroSorteado: do tipo number, que contém o número sorteado
+pessoasConcorrendo: um array de objetos, que contém os dados das pessoas que estão concorrendo ao sorteio. 
+Cada objeto deste array possui duas propriedades:
+nome: do tipo string, que contém o nome da pessoa
+numeroCartela: do tipo number, que contém o número da cartela comprada
+Sua função deve RETORNAR:
+
+o nome da pessoa, caso alguém tenha ganhado
+undefined, caso ninguém tenha ganhado (o número sorteado não foi encontrado em nenhuma das cartelas)
+Para evitar erros de implementação, você deve tipar as entradas e o retorno da função.
+
+DICA: crie um tipo "pessoa".
+*/
+
+type TPessoa = {
+    nome: string,
+    numeroCartela: number
+}
+
+function verificarPremiado(numeroSorteado: number, pessoasConcorrendo: TPessoa[]){
+    for(let i = 0; i < pessoasConcorrendo.length; i++){
+        if(numeroSorteado === pessoasConcorrendo[i].numeroCartela){
+            return pessoasConcorrendo[i].nome
+        }
+    }
+    return undefined
+}
 
 export default verificarPremiado;
